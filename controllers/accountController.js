c48cfe152a35cd3edbe680f85ee0f463fb3789b1
antoniodeca88 <78661681+antoniodeca88@users.noreply.github.com>
2025-05-29 20:12:19 -0600

    password error fixed

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -29,6 +29,7 @@ async function buildRegister(req, res, next) {
 async function registerAccount(req, res) {
     let nav = await utilities.getNav()
     const { account_firstname, account_lastname, account_email, account_password } = req.body
+    console.log("Datos recibidos:", account_firstname, account_lastname, account_email)
   
     const regResult = await accountModel.registerAccount(
       account_firstname,
